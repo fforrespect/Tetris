e8f5db94778d7d8c216ce433dc11eada8cd79099
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-08 17:28:39 +0400

    -m Rearranged logic of collision detection

--- source/Game/Board.py
+++ source/Game/Board.py
@@ -1,22 +1,6 @@
-from Setup import Constants, GlobalVars
+from Setup import Constants
 
 
 class Board:
     def __init__(self):
         self.grid = [[" " for __ in range(Constants.GRID_SIZE[0])] for _ in range(Constants.GRID_SIZE[1])]
-
-    def adjust_velocities_for_collisions(self, l_or_r: int, down: int) -> tuple[int, int]:
-        l_or_r: int = l_or_r
-        down: int = down
-
-        # Hit left or right wall
-        for x_pos in map(lambda x: x[0] + l_or_r, GlobalVars.active_tetromino.get_all_pos()):
-            if not (0 <= x_pos < Constants.GRID_SIZE[0]):
-                l_or_r = 0
-
-        # Hit floor
-        for y_pos in map(lambda x: x[1] + down, GlobalVars.active_tetromino.get_all_pos()):
-            if y_pos >= Constants.GRID_SIZE[1]:
-                down = 0
-
-        return l_or_r, down
--- source/Game/Tetromino.py
+++ source/Game/Tetromino.py
@@ -63,7 +63,7 @@ class Tetromino:
 
         # rotate: int = 0
 
-        l_or_r, down = GlobalVars.game_board.adjust_velocities_for_collisions(l_or_r, down)
+        l_or_r, down = self.__adjust_velos_for_collisions(l_or_r, down)
 
         self.nw_pos[0] += l_or_r
         self.nw_pos[1] += down
@@ -75,3 +75,19 @@ class Tetromino:
                 if self.matrix[row][col] == 1:
                     all_pos.append([(self.nw_pos[0] + col), (self.nw_pos[1] + row)])
         return all_pos
+
+    def __adjust_velos_for_collisions(self, l_or_r: int, down: int) -> tuple[int, int]:
+        l_or_r: int = l_or_r
+        down: int = down
+
+        # Hit left or right wall
+        for x_pos in map(lambda x: x[0] + l_or_r, self.get_all_pos()):
+            if not (0 <= x_pos < Constants.GRID_SIZE[0]):
+                l_or_r = 0
+
+        # Hit floor
+        for y_pos in map(lambda x: x[1] + down, self.get_all_pos()):
+            if y_pos >= Constants.GRID_SIZE[1]:
+                down = 0
+
+        return l_or_r, down
