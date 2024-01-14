4e791f58932251b35d997c15fe37a05368e0d712
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-14 09:48:49 +0000

    Adjusted topout logic, for later use

--- source/Event/GameOver.py
+++ source/Event/GameOver.py
@@ -1,11 +1,12 @@
 import sys
 import pygame
 
+from Event import ToppedOut
 from Process import HighScore
 from Setup import GlobalVars
 
 
-def _quit_all():
+def quit_all():
     HighScore.add_new_score()
     print(GlobalVars.game_board)
     print("Score:", GlobalVars.score)
@@ -19,9 +20,8 @@ def _quit_all():
 def quit_pressed(events):
     for event in events:
         if event.type == pygame.QUIT:
-            _quit_all()
+            quit_all()
 
 
 def top_out():
-    print("Topped out!")
-    _quit_all()
+    ToppedOut.run()
---
+++ source/Event/ToppedOut.py
@@ -0,0 +1,6 @@
+from Event import GameOver
+
+
+def run():
+    print("Topped out!")
+    GameOver.quit_all()
