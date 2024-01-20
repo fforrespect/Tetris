43493f5fc7c03306fc43439f5d1bfab6dc81f54b
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-20 16:47:35 +0000

    Edited ResizeImage.py to be more functional

--- source/Tools/ResizeImage.py
+++ source/Tools/ResizeImage.py
@@ -1,16 +1,19 @@
 from PIL import Image
 
 
-def resize_with_aliasing(image_fp: str, target_fp: str) -> None:
+def resize(image_fp: str, target_fp: str, factor: float, anti_aliasing: bool = False) -> None:
     old_image = Image.open(image_fp)
-    new_size = tuple(map(lambda x: round(x*.75), old_image.size))
+    new_size = tuple[int, int](map(lambda x: round(x*factor), old_image.size))
 
     # Resize the image with aliasing using Image.Resampling.NEAREST
-    new_image = old_image.resize(new_size, Image.Resampling.NEAREST)
+    resample = Image.Resampling.LANCZOS if anti_aliasing else Image.Resampling.NEAREST
+    new_image = old_image.resize(new_size, resample)
 
     # Save the resized image
     new_image.save(target_fp)
 
 
-resize_with_aliasing("../../Resources/Images/Blocks/Classic/blocks.png",
-                     "../../Resources/Images/Blocks/Classic/blocks_new.png")
+resize("/Users/jaiveer/Downloads/smile.png",
+       "/Users/jaiveer/Downloads/smile.png",
+       0.5,
+       False)
